c3c25174063ba60a0274fd43a888fd281350ba18
Konstantin Senchenkov <senchenkovko@gmail.com>
2024-01-26 20:58:50 +0400

    Create new HW

---
+++ HW_06/binary_search.py
@@ -0,0 +1,12 @@
+def binary_search(arr, x):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] < x:
+            left = mid + 1
+        elif arr[mid] > x:
+            right = mid - 1
+        else:
+            return mid
+    return -1
